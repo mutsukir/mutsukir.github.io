c2b26d87cd0d4a74ff736c47c042e9cac56448c7
mutsukir <53193187+mutsukir@users.noreply.github.com>
2024-08-21 07:49:00 +0800

    Update business_card.js

--- business_card.js
+++ business_card.js
@@ -66,8 +66,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const mindarThree = new MindARThree({
       container: document.body,
       imageTargetSrc: 'assets/card_back.mind',
-      filterMinCF: 0.000001, filterBeta: 1,
-      missTolerance: 30
+      filterMinCF: 0.0000001, filterBeta: 1,
+      missTolerance: 15
     });
     const {renderer, cssRenderer, scene, cssScene, camera} = mindarThree;
 
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add cake
     const cake = await loadGLTF("cake/scene.gltf"); // Credit - 3D model by Warkarma (https://sketchfab.com/warkarma)
-    cake.scene.scale.set(10, 10, 10);
+    cake.scene.scale.set(5, 5, 5);
     cake.scene.position.set(0, 0, 0);
     cake.scene.rotation.x = Math.PI/2;
     cake.scene.rotation.y = -Math.PI;
@@ -96,16 +96,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add Big board
     const texture_main = new THREE.TextureLoader().load('textures/027-08_main.png' ); 
     const material_main = new THREE.MeshBasicMaterial( { map:texture_main, transparent: true, opacity: 1.0, color: 0xFFFFFF } );
-    const geometry_main = new THREE.PlaneGeometry(2.5, 3.8);
+    const geometry_main = new THREE.PlaneGeometry(1.25, 1.9);
     const plane_main = new THREE.Mesh(geometry_main,material_main);
     plane_main.rotation.x = Math.PI/3;
-    plane_main.position.set(0,2.5,1.5);
+    plane_main.position.set(0,1.25,0.75);
     anchor.group.add(plane_main);
 
     // Add lace under cake
     const texture_lace = new THREE.TextureLoader().load('textures/lace.png' ); 
     const material_lace = new THREE.MeshBasicMaterial( { map:texture_lace, transparent: true, opacity: 1.0, color: 0xa182b8 } );
-    const geometry_lace = new THREE.PlaneGeometry(3, 3);
+    const geometry_lace = new THREE.PlaneGeometry(1.5, 1.5);
     const plane_lace = new THREE.Mesh(geometry_lace, material_lace);
     anchor.group.add(plane_lace);
 
@@ -124,7 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const video_geometry = new THREE.PlaneGeometry(1.2, 1080/1920*1.2);
     const video_material = new THREE.MeshBasicMaterial({map: video_texture});
     const video_plane = new THREE.Mesh(video_geometry, video_material);
-    video_plane.position.set(1.8, -0.1, 0.2);
+    video_plane.position.set(1.2, -0.05, 0.1);
     video_plane.rotation.x = Math.PI/3;
     anchor.group.add(video_plane);
     video.addEventListener('play', () => {
@@ -136,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const material_button = [];
     const plane_button = [];
     //const geometry_button = new THREE.PlaneGeometry(0.5, 0.5);
-    const geometry_button = new THREE.PlaneGeometry(1, 1);
+    const geometry_button = new THREE.PlaneGeometry(0.5, 0.5);
     let selected = 'none';
 
     for (let i=0; i<texture_button.length;i++){
@@ -144,10 +144,10 @@ document.addEventListener('DOMContentLoaded', () => {
       plane_button[i] = new THREE.Mesh(geometry_button,material_button[i]);
       plane_button[i].rotation.x = Math.PI/3;
       if (i<texture_button.length/2){
-        plane_button[i].position.set(-1.5-1*i, i%(texture_button.length/2), 1.5);
+        plane_button[i].position.set((-1.5-1*i)/2, (i%(texture_button.length/2)+0.2), 0.75);
       }
       else{
-        plane_button[i].position.set(1.5+(1*(i-2)), i%(texture_button.length/2), 1.5);
+        plane_button[i].position.set((1.5+(1*(i-2)))/2, (i%(texture_button.length/2)+0.2), 0.75);
       }
       plane_button[i].userData.clickable = true;
 
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const text = document.querySelector("#ar-div");
     const css_plane = new CSS3DObject(text);
     css_plane.rotation.x = Math.PI/3;
-    css_plane.position.set(-1500, 0, 0);
+    css_plane.position.set(-1000, 0, 0);
 
     const cssAnchor = mindarThree.addCSSAnchor(0);
     cssAnchor.group.add(css_plane);
