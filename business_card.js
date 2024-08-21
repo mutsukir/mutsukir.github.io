6109d727e52a59defe54fd3a1a0a0ee0eb1bd3c6
mutsukir <53193187+mutsukir@users.noreply.github.com>
2024-08-21 11:45:04 +0800

    Update card

--- business_card.js
+++ business_card.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new MindARThree({
       container: document.body,
-      imageTargetSrc: 'assets/ptcg03.mind',
+      imageTargetSrc: 'assets/card_back.mind',
       filterMinCF: 0.0000001, filterBeta: 1,
       missTolerance: 15
     });
