93d46f05dcdbb6daa4a5742a3170ad9aee7f9569
mutsukir <53193187+mutsukir@users.noreply.github.com>
2024-11-07 17:08:25 +0800

    Add pusheen

--- furniture.js
+++ furniture.js
@@ -71,8 +71,8 @@ document.addEventListener('DOMContentLoaded', () => {
       document.body.appendChild(renderer.domElement);
       document.body.appendChild(arButton);
   
-      const itemNames = ['lillie', 'gura', 'destiny_ghost'];
-      const itemHeights = [3.0, 4.0, 0.1];
+      const itemNames = ['lillie', 'gura', 'pusheen'];
+      const itemHeights = [3.0, 3.0, 1.0];
       const items = [];
       for (let i = 0; i < itemNames.length; i++) {
         const model = await loadGLTF('assets/furniture/' + itemNames[i] + '/scene.gltf');
